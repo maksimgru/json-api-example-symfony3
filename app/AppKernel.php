00db3f9fbc50e5e4cb843a9b4dc3fc13cf17232b
Maksim Petrenko <maksimgru@gmail.com>
2018-04-13 15:43:07 +0300

    Install Doctrine Migrations Bundle;

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
 
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle(),
         ];
 
